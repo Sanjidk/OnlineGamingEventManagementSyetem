d7c63c11303a6ca51f020200a1158e3c95253de5
Sanjid <sanjidk24@gmail.com>
2021-03-24 20:59:28 +0600

    sfb

---
+++ resources/views/backend/partial/footer.blade.php
@@ -0,0 +1,7 @@
+<!-- partial:partials/_footer.html -->
+<footer class="footer">
+          <div class="d-sm-flex justify-content-center justify-content-sm-between">
+            <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © 2021 Online Gaming Event Management System by SANJID</span>
+          </div>
+        </footer>
+        <!-- partial -->
